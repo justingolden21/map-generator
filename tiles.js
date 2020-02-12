3673d5e2dd871ec3839b46ebe8280aeb50b484c7
RGB Studios <justingolden21@gmail.com>
2020-02-12 03:36:12 -0800

    fix bug 3
    bug 3 was for water surrounded by exactly 3 pieces of dirt. there were no sprites. added sprites (made by combining corner sprites)

--- tiles.js
+++ tiles.js
@@ -49,18 +49,20 @@ function getTile(x, y, width, height) {
 	// }
 
 	// dirt on 3 sides
+	// start bug fix 3
 	if(dirt_above && dirt_right && dirt_below) {
-		// return [4,1];
+		return [0,7];
 	}
 	if(dirt_right && dirt_below && dirt_left) {
-		// return [5,1];
+		return [1,7];
 	}
 	if(dirt_below && dirt_left && dirt_above) {
-		// return [6,1];
+		return [2,7];
 	}
 	if(dirt_left && dirt_above && dirt_right) {
-		// return [7,1];
+		return [3,7];
 	}
+	// end bug fix 3
 
 	// dirt on 2 sides
 	if(dirt_above && dirt_right) {
@@ -190,9 +192,11 @@ function drawTileGrid(width, height) {
 /*
 test condition maps:
 
+bugs 1, 2:
 importMap("01000060/////////x/////////g/////////g/////////A////////+B////////+D/////////H/Pj////8//+HB///+Af/+AA///8AP/+AAf//8AH/+AAP//8AA/+AAP//8AAf/AAf/H8AAP/gAf+D4AAP/gAP+D4AAH/gAH+D4AAD/AAH/D4APB/AAD/j4Afg+AAD/x8A/wgAAD/w+A/wAAAD/4fA/gAAAB/4fAfggAAA/wfAfw4AAA/g/APw8AAA/B/APx8AAAeB+Afx+AAAOB8A/w/AAAPA4A/w/gAAfAAAf5/gAAfgAAP//gDAPgAAH//gHgHAAAH//gHgAAAAH//wDwAAAAD//8D4AAAAA///n8AAAAAP/n/+AAAAAD/Af/AAAAAB/AD/gAYAAB/AB/wA8AAB/AAHwA8AAB/AADgB+AAB/AAAAf+AAA/gAAA/8AAA/gAAA/4AAA/AAAA/wAAA8AAMAfwAAB4AAfgfwAAB4AAf8/wAAAwAAf//gAAAgAAf//hgAAgAAf//DwAAgAAf/+DwAAgAAfn8B4AAgAAfD8A8AAAAAPB8A/GAAAAPB8B//AgAAPB8H//A4AAGA4P//h8AAAAAP//z8AAAAAH///8AAAAAH///+AAAAAH////MAAAAP////+AAAAP/////AAAAP///z/gAAAPP//g/wAAAGH//gfwAAAGA//wfgAAAHAf/4PAAAAHgA/4HAAAADwAf4HgAAADwAf8H4AAABgAP+H8AAAAAAH+D8AAAAAAH/D8AAAAAAD/n4AAAAAAD//4AAAAAAD//8AAAAAAB//+AAAAAAB///AAAAAAB///gADAAAB///gAHgAAB///gAHgAAA///gAHgAAA///gAHwAA5///gGD4AB////gPB8AB////gPg8AB////gHweAA//P/AH4eAA/+H8AP8/MAf+D4Af//+AP+B4Af//+AH/A4Af//+AH/A8Af//+AP/g+A////AP/5//////4f")
 drawTileGrid(100,60)
 
+bug 3:
 importMap("01000060////////////////////////////////+f////////8P////////8P////////8H////////8H///////z+D//+H///h/AP/8D4f/A/gD/8AwP/AD4B/4AAH/AB8B/wAAD/AA+A/wADD/gAfAPwAHn/gAfAHwAP//gM/ADgAf//gf/AAAA///wf/gAAA///wf/gAAA///4f/gAAB///4P/AAABz//4H/AAABh//4H/AAADgH/4H+AAAPgD/4P8AAAfAD/4P8AAAeAD/wH8AAAOAH/wB+AAAOAH/gA+AAAeAH/gA/AAAcAH/gA/AAA4AP/gA/AABwAP/AA+AABwAP+AA+AAB4AH+AB/AAB4AH+AB/gAB8AD+AD/gAB8AB/AD/AAA4AA/wB+AAAAAA/4B+AAAAAB/4D+AAAAAD/4D8AAAAAH/4B4AAAAAH/8AAAAAAAH/+AAAAGAAD//AAAAPAAB//AAAAPgAB//AAAAPwAB/+AAAAPwAD/8AAAAPwAD/4AAAAPwAD/wAAAAHwAD/wAAAAHgAB/wAAYADAAB/gAA8AAAMB+AAA8AAAeA8AAA8BgAfA8AAA8DwAfA+AAB8D4AfA+AAD8B8AOB8AAH8B/4AB8AAH8B/8AB8AAH4A/8AB8AAD4Af8AB8AAB4AP4AB8AAB4AH4AD8AAB4AD4AD+AAD4AD4AD+AAD4AD4AD/AAB4ABwHB/wAA4AAAPg/4AAwAAAfg/4ABwAAA/g/4ABwAAA/x/4AA4AAAf//8AAQAAAP//+AAAAAAH/8/AAAAAAB/4fAAAAAAA/wfAAAAAAA/w+AAAAAAB/x8AAAAAAB/58AAAAAAD/44AAAAAAH/8AAAAAAAH/+AAAAAAAH//AAYAAAAH//AA8AAAAH//AA8AAAAD//AAYAAAAA/+AAAAAAAAf+AAAAcAAAf+AAAA+AAAP+AAAA+AAAD8AAAA+AAAB8AAAAfAAAB8AAAAfgAAB+AADg/wABj/4AHz//gf/")
 drawTileGrid(100,60)
 */
